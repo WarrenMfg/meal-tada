46100c5c3d5756c0068061d9845301040243a732
WarrenMfg <warrenmfg@icloud.com>
2020-08-09 17:18:23 -0400

    seed test

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,10 @@
 export default (app, db) => {
-  app.get('/api/hello-world', async (req, res) => {
+  app.post('/api/seed/recipes', async (req, res) => {
     try {
-      res.send({ hello: 'world' });
+      const newRecipe = await db.collection('recipes').insertOne(req.body);
+      res.send(newRecipe.ops[0]);
     } catch (err) {
+      res.status(400);
       console.log(err.message, err.stack);
     }
   });
